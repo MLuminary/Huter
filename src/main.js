6b8f3fbbca87c528129cdd8a9501c87cba03e9f3
MLuminary <418037025@qq.com>
2018-08-27 22:49:25 +0800

    UI框架改为 ElementUI

--- src/main.js
+++ src/main.js
@@ -1,58 +1,15 @@
-import Vue from 'vue';
-import iView from 'iview';
-import VueRouter from 'vue-router';
-import Routers from './router';
-import Vuex from 'vuex';
-import Util from './libs/util';
-import App from './app.vue';
-import 'iview/dist/styles/iview.css';
-import '../my-theme/index.less'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 
-Vue.use(VueRouter);
-Vue.use(Vuex);
+Vue.config.productionTip = false
 
-Vue.use(iView);
-
-
-
-// 路由配置
-const RouterConfig = {
-    mode: 'history',
-    routes: Routers
-};
-const router = new VueRouter(RouterConfig);
-
-router.beforeEach((to, from, next) => {
-    iView.LoadingBar.start();
-    Util.title(to.meta.title);
-    next();
-});
-
-router.afterEach(() => {
-    iView.LoadingBar.finish();
-    window.scrollTo(0, 0);
-});
-
-
-const store = new Vuex.Store({
-    state: {
-
-    },
-    getters: {
-
-    },
-    mutations: {
-
-    },
-    actions: {
-
-    }
-});
-
-
+/* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    router: router,
-    store: store,
-    render: h => h(App)
-});
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
