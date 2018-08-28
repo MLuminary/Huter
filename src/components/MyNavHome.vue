34c6f8b367e84b1303f73f7b51ee2012bed8e8d8
MLuminary <418037025@qq.com>
2018-08-28 20:14:14 +0800

    修改elementui主题

--- src/components/MyNavHome.vue
+++ src/components/MyNavHome.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Welcome</h1>
+  <div>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -12,5 +21,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ELementUI from 'element-ui'
+import '../theme/index.css'
 
 Vue.config.productionTip = false
+
+Vue.use(ELementUI)
 
 /* eslint-disable no-new */
 new Vue({
